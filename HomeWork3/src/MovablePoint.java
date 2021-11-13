c4482386e22213810025f73c4374cf9e75b2471c
Jevann <vanoid22@gmail.com>
2021-11-13 20:53:11 +0300

    HomeWork3
    HomeWork3

---
+++ HomeWork3/src/MovablePoint.java
@@ -0,0 +1,43 @@
+package homework3;
+
+public class MovablePoint implements homework3.Movable
+{
+
+    int x;
+    int y;
+    int xSpeed;
+    int ySpeed;
+
+
+public MovablePoint(int x, int y, int xSpeed, int ySpeed)
+{
+    this.x = x;
+    this.y = y;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+}
+public String toString()
+{
+    return "x = " + x + ", y = " + y + ", xSpeed = " + xSpeed + ", ySpeed = " + ySpeed;
+}
+    @Override
+    public void moveUp()
+    {
+        y += ySpeed;
+    }
+    @Override
+    public void moveDown()
+    {
+        y -= ySpeed;
+    }
+    @Override
+    public void moveLeft()
+    {
+        x -= xSpeed;
+    }
+    @Override
+    public void moveRight()
+    {
+        x += xSpeed;
+    }
+}
