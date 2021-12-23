9491f209c0f26509370b2bb60876549f1942f2c3
Jevann <vanoid22@gmail.com>
2021-12-23 03:10:34 +0300

    Homework7

---
+++ HomeWork7/src/Main.java
@@ -0,0 +1,11 @@
+package homework7;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        homework7.onQueue onQueue = new onQueue();
+        homework7.onDequeue dequeue = new homework7.onDequeue();
+        homework7.onStack onStack = new onStack();
+    }
+}
