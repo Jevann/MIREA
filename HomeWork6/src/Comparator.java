30c1a8c9a93fc8a0887724510907afa84e4a9f49
Jevann <vanoid22@gmail.com>
2021-12-23 03:10:24 +0300

    Homework6

---
+++ HomeWork6/src/Comparator.java
@@ -0,0 +1,6 @@
+package homework6;
+
+public interface Comparator
+{
+   void QuickSort(int[] source, int leftBorder, int rightBorder);
+}
---
+++ HomeWork6/src/SortingStudentsByGPA.java
@@ -0,0 +1,39 @@
+package homework6;
+
+public class SortingStudentsByGPA implements homework6.Comparator
+{
+    public void QuickSort(int[] mass, int low, int high)
+    {
+        if (mass.length == 0) {
+            return;
+        }
+        if (low >= high) {
+            return;
+        }
+        int middle = low + (high - low) / 2;
+        int a = mass[middle];
+        int i = low, j = high;
+        while (i <= j)
+        {
+            while (mass[i] > a) {
+                i++;
+            }
+            while (mass[j] < a) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = mass[i];
+                mass[i] = mass[j];
+                mass[j] = temp;
+                i++;
+                j--;
+            }
+            if (low < j) {
+                QuickSort(mass, low, j);
+            }
+            if (high > i) {
+                QuickSort(mass, i, high);
+            }
+        }
+    }
+}
